aa1ecd0b75aac9e2a424b1839450f00036199842
Jason Stewart <jaysin.stewart@gmail.com>
2023-04-11 11:49:50 -0700

    Completed js events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+// truning the 'dodger' into a variable
+const dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = '#32CD32'
+
+dodger.style.bottom = '0px'
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight () {
+    const rightNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(rightNumbers, 10)
+
+    if (left < 360)
+        dodger.style.left = `${left + 1}px`
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight()
+    }
+})
